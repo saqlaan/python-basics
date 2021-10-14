8fd027b15994c02bf74beff446fd0f92a240eb51
saqlaan <saqlain.awan@arbisoft.com>
2021-10-14 16:44:59 +0500

    Variables and types

---
+++ variable-types/script.py
@@ -0,0 +1,37 @@
+myNum = 8
+print(myNum)
+
+myFloat = 2.0
+print(myFloat)
+myFloat = float(7)
+print(myFloat)
+
+myStr = 'hello'
+print(myStr)
+myStr = "hello"
+print(myStr)
+
+myStr = "Don't say anything"
+print(myStr)
+
+# Multi assignments
+a, b = 3, 4
+print(a)
+print(b)
+
+# Mixed operations not allowed
+# print(5 + '10')
+# print('5' + 10)
+print(5 + 10)
+
+# Excersice
+mystring  = "hello"
+myfloat = 10
+myint = 20
+
+if mystring == "hello":
+  print("String: %s" %mystring)
+if myfloat == 10.0:
+  print("Float: %f" %myfloat)
+if myint == 20:
+  print("IntegerL: %d" %myint)
